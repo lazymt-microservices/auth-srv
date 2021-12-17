444db61bd1a5bd2cb011ea6e010795089870a2e2
lazymt <misha_trunov@hotmail.com>
2021-12-17 18:02:56 +0200

    updated deps

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,15 @@ const start = async () => {
   }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    });
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
+    console.log('Listening on port 3000');
   });
 };
 
-start();
+start().then(console.log);
