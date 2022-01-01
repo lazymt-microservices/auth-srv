916618db841d8fe17e4c6011c7b74993a5c69187
lazymt <misha_trunov@hotmail.com>
2022-01-01 23:08:35 +0200

    use npm package instead of local files

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@lazymtlib/common';
 
 const router = express.Router();
 
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+import { validateRequest, BadRequestError } from '@lazymtlib/common';
 
-import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -43,7 +42,7 @@ router.post(
 
     // Store it on session object
     req.session = {
-      jwt: userJwt
+      jwt: userJwt,
     };
 
     res.status(201).send(user);
